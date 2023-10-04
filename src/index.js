fc3ad23b40cac1695f44096cd25191852c02744e
Seemabhattarai <simabhattaraibca077@indrenicollege.edu.np>
2023-10-04 14:02:34 +0545

    first

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Counter from './pages/Counter';
+import About from './pages/About';
+import Service from './pages/Service';
+import Contact from './pages/Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/counter" element={<Counter />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/service" element={<Service />} />
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
